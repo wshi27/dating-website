0ff07716b56faa3f27dd8ad23154bc6a85fd9ecd
Yuelin Zhao <yuelinzhao@Yuelins-iMac.lan>
2021-11-13 22:45:23 -0500

    setup prettier+eslint+husky

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ok now?!
-        </p>
-    
-
+        <p>ok now?!</p>
       </header>
     </div>
   );
