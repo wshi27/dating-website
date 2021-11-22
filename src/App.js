8841bf587925263f425ab2deae0e08d3c5c5fe1b
changlc125 <84114639+changlc125@users.noreply.github.com>
2021-11-21 22:35:03 -0500

    Testing1 (#5)
    * second
    
    * fixed
    
    * setup prettier+eslint+husky (#3)
    
    * setup prettier+eslint+husky
    
    * delete a un-necessary line of code to address PR review
    
    Co-authored-by: Yuelin Zhao <yuelinzhao@Yuelins-iMac.lan>
    
    * Header and Footer done
    
    Co-authored-by: wshi27 <wshi27@hotmailcom>
    Co-authored-by: Weiwei <wshi27@hotmail.com>
    Co-authored-by: howareyoucolin <howareyoucolin@gmail.com>
    Co-authored-by: Yuelin Zhao <yuelinzhao@Yuelins-iMac.lan>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header/Header.js";
+import "./Header/Header.css";
+import Main from "./Main/Main.js";
+import Footer from "./Footer/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Main/MemberList/MemberList.js
@@ -0,0 +1,22 @@
+import "./MemberList.css";
+const member = pros => {
+  return (
+    <div class="member_list">
+      member
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default member;
